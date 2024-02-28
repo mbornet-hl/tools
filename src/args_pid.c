da360c20519236d0fb8bf7e3410dabc66afbbb3f
mb <mbornet.pro@wanadoo.fr>
2024-02-28 23:27:55 +0100

    Updated license : GPL v3

--- src/args_pid.c
+++ src/args_pid.c
@@ -1,17 +1,26 @@
 /* ============================================================================
- *   (C) Copyright Martial Bornet, 2014.
+ * Copyright (C) 2010-2024, Martial Bornet
  *
- *   Auteur       :     Martial BORNET (MB) - 2 Juillet 2014
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
  *
- *   Description  :     Affichage des arguments passes en parametres
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *   Fichier      :     aj_args.c
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *   Author       :     Martial BORNET (MB) - July 1, 2010
+ *
+ *   File         :     aj_args_pid.c
  *
  *   @(#)  aj_args_pid.c  1.2  22/09/10  MB  
  *
- *   Liste des fonctions de ce fichier :
- *   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- *   - main
  * ============================================================================
  */
 
